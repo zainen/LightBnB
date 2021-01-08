9e4eb5a7e52506dcd5cb279bcfd1dc079c1c4ede
zainen <zainensuzuki@gmail.com>
2021-01-08 00:25:13 +0000

    getAllProperties js->sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -119,10 +119,42 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  let queryParams = []
+  let queryString = `
+  SELECT properties.*, avg(rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
+  `;
+  if (options.city){
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length}
+    `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+      queryString += ` AND cost_per_night >= $${queryParams.length}
+      `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+      queryString += ` AND cost_per_night <= $${queryParams.length}
+      `;
+  }
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` AND rating >= $${queryParams.length}
+    `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  console.log(queryString, queryParams)
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 // const getAllProperties = function(options, limit = 100) {
